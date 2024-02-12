cd91dd7ec8304449ab35ca9230afa4ca1bcd7a0c
monsosam <monsonsamuel5@gmail.com>
2024-02-11 22:56:48 -0700

    lkmju

--- src/routes/htmlR.js
+++ src/routes/htmlR.js
@@ -9,12 +9,12 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Serve the notes.html page
 router.get('/notes', function(req, res) {
-  res.sendFile(join(__dirname, '../public/notes.html'));
+  res.sendFile(join(__dirname, '../../public/notes.html'));
 });
 
 // Catch-all route to serve the index.html page
 router.get('*', function(req, res) {
-  res.sendFile(join(__dirname, '../public/index.html'));
+  res.sendFile(join(__dirname, '../../public/index.html'));
 });
 
 export default router;
